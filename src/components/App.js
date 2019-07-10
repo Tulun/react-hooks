4b492ce92230f19376f531e27970394e094f0768
Jason Kiraly <jaykiraly@gmail.com>
2019-07-10 16:22:28 -0700

    button for posts/todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {resource: "posts" }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: "posts" })}>Posts</button>
+          <button onClick={() => this.setState({ resource: "todos" })}>Todos</button>
+        </div>
+        {this.state.resource}
+      </div>
     )
   }
 }
